21a52c7a72ca5e33f965414cdf2e2573082e6438
Artem Efimov <tomhill@yandex.ru>
2023-02-28 21:18:33 +0100

    Add cosmos client

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using ImageGram.Storage;
+using ImageGram.Repository;
+using ImageGram.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<Options>(builder.Configuration.GetSection("CosmosDB"));
+builder.Services.AddSingleton<IStorage, CosmosStorage>();
+builder.Services.AddScoped<IRepository, CosmosRepository>();
+builder.Services.AddScoped<ICDBService, CosmosService>();
 
 var app = builder.Build();
 
---
+++ Repository/IRepository.cs
@@ -0,0 +1,7 @@
+namespace ImageGram.Repository;
+
+public interface IRepository {
+    public bool Check();
+
+    public string GetData();
+}
